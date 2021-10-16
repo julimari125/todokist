59b46fa390b52ff371b6aa98991153b4b6f3eec8
IWATA JULIANA <mari.i@gainsline.com>
2021-10-17 00:03:28 +0900

    create DB

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.datastructures import ContentRange
+from forms import Todo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'password'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class TodoModel(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    content = db.Column(db.String(240))
+
+    def __str__(self):
+        return f'{self.content}, {self.id}'
+
+@app.route('/', methods=['GET','POST'])
 def hello_world():
-    return "<h1>Hello Worlds</h1>"
+    request_method = request.method
+    todo = TodoModel.query.all()
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        return redirect(url_for('name',first_name = first_name))
+    return render_template('hello.html', request_method = request_method, todo=todo)
+
+@app.route('/name/<string:first_name>')
+def name(first_name):
+    return f'{first_name}'
+
+@app.route('/todo', methods = ['GET', 'POST'])
+def todo():
+    todo_form = Todo()
+    if todo_form.validate_on_submit():
+        todo = TodoModel(content = todo_form.content.data)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    return render_template('todo.html', form = todo_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
